499d7f37dbc4067238ce0fac6394778ebf05280e
onlydcx <rin.hanaoka@icloud.com>
2024-03-26 22:50:49 +0900

    [add] app_main

---
+++ Core/App/app_main.cpp
@@ -0,0 +1,9 @@
+/*
+ *  app_main.cpp
+ *
+ *  Created on: 2024/3/36
+ * 
+ *  Author: onlydcx
+ */
+
+#include <app_main.h>
---
+++ Core/App/app_main.h
@@ -0,0 +1,15 @@
+/*
+ *  app_main.h
+ *
+ *  Created on: 2024/3/36
+ * 
+ *  Author: onlydcx
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" void app_main();
+#else
+void app_main();
+#endif
