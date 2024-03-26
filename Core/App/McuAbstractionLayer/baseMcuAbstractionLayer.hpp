be8d2e864f926cb95f4d9e4218721ddbd3eb14cf
onlydcx <rin.hanaoka@icloud.com>
2024-03-26 22:51:08 +0900

    [add] basemcu layer file

---
+++ Core/App/McuAbstractionLayer/baseMcuAbstractionLayer.hpp
@@ -0,0 +1,17 @@
+/*
+ *  baseMcuAbstractionLayer.hpp
+ *
+ *  Created on: 2024/3/36
+ * 
+ *  Author: onlydcx
+ */
+
+#pragma once
+
+#include <cstring>
+#include <stdint.h>
+#include <math.h>
+
+class baseMcuAbstractionLayer{
+
+};
---
+++ Core/App/McuAbstractionLayer/stm32f767AbstractionLayer.hpp
@@ -0,0 +1,7 @@
+/*
+ *  stm32f767AbstractionLayer.hpp
+ *
+ *  Created on: 2024/3/36
+ * 
+ *  Author: onlydcx
+ */
